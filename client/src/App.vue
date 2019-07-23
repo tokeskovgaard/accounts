2fa5f90976987b2fe07717aad21dd4540d63e0ba
Toke Reimer Skovgaard <toke@reimerskovgaard.dk>
2019-07-23 22:29:13 +0200

    WOrking on the front

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
     <v-content>
-      <router-view />
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm6 md3 elevation-12 class="pa-4">
+            <router-view />
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
